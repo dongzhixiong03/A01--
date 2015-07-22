f9762b6207f7f7cc031c1a631441d3b3c45ce9ee
傅红雪 <@qq.com>
2015-07-22 08:09:25 +0800

    Initial Commit

---
+++ A01-网易新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  A01-网易新闻
+//
+//  Created by teacher on 15/7/22.
+//  Copyright (c) 2015年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ A01-网易新闻/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  A01-网易新闻
+//
+//  Created by teacher on 15/7/22.
+//  Copyright (c) 2015年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
